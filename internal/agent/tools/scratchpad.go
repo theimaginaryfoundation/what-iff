afdce833833edf8927f11452909ee72ade4d72ef
dahmonson-ux <dahmonson@gmail.com>
2026-08-30 14:16:54 -0500

    Make scratchpad handler datastore fakeable

--- internal/agent/tools/scratchpad.go
+++ internal/agent/tools/scratchpad.go
@@ -1,12 +1,21 @@
 package tools
 
 import (
+	"context"
+
+	"github.com/google/uuid"
 	"github.com/theimaginaryfoundation/what-iff/internal/datastore"
+	"github.com/theimaginaryfoundation/what-iff/internal/models"
 	"go.uber.org/zap"
 )
 
+type scratchpadDatastore interface {
+	GetPersonality(ctx context.Context, userID, id uuid.UUID) (*models.Personality, error)
+	UpdatePersonalityScratchpad(ctx context.Context, userID uuid.UUID, personalityModel models.Personality) (*models.Personality, error)
+}
+
 type ScratchpadTool struct {
-	datastore *datastore.Datastore
+	datastore scratchpadDatastore
 	logger    *zap.Logger
 }
 
