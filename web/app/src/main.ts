908e88361b208db130c702b95c99988b7859c21a
dahmonson-ux <dahmonson@gmail.com>
2026-08-30 13:12:03 -0500

    Enable composer emoji shortcodes

--- web/app/src/main.ts
+++ web/app/src/main.ts
@@ -1,9 +1,12 @@
+import { EmojiSearch } from '@ctrl/ngx-emoji-mart';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { ThemeService } from './app/core/services/theme.service';
+import { installComposerEmojiShortcodes } from './app/features/chat/helpers/emoji-shortcode.helpers';
 
 ThemeService.initializeThemeEarly();
 
 bootstrapApplication(App, appConfig)
+  .then(appRef => installComposerEmojiShortcodes(appRef.injector.get(EmojiSearch)))
   .catch((err) => console.error(err));
