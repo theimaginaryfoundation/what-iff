b2ccfc78402d33b79ab8ac2fbb372c503308017c
dahmonson-ux <dahmonson@gmail.com>
2026-08-31 15:40:47 -0500

    Test production-representable import ordering step

--- internal/datastore/chat_import_order_test.go
+++ internal/datastore/chat_import_order_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestNormalizeImportedMessageTimesPreservesOrder(t *testing.T) {
-	base := time.Date(2025, 8, 3, 3, 0, 2, 0, time.UTC)
+	base := time.Date(2025, 8, 3, 3, 0, 2, 123456000, time.UTC)
 	messages := []models.ChatMessage{
 		{Message: "first", Origin: models.MessageOriginUser, SentAt: base},
 		{Message: "second", Origin: models.MessageOriginAssistant, SentAt: base},
@@ -19,8 +19,8 @@ func TestNormalizeImportedMessageTimesPreservesOrder(t *testing.T) {
 	normalized := normalizeImportedMessageTimes(messages)
 
 	require.Equal(t, base, normalized[0].SentAt)
-	require.Equal(t, base.Add(time.Nanosecond), normalized[1].SentAt)
-	require.Equal(t, base.Add(2*time.Nanosecond), normalized[2].SentAt)
+	require.Equal(t, base.Add(time.Microsecond), normalized[1].SentAt)
+	require.Equal(t, base.Add(2*time.Microsecond), normalized[2].SentAt)
 	require.Equal(t, base, messages[1].SentAt)
 	require.Equal(t, base.Add(-time.Second), messages[2].SentAt)
 }
